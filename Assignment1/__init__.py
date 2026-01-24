3778dd7e7d1fa1aabe7d3c9f43c42dedaef0fa22
gouzigouzi <3113700669@qq.com>
2026-01-24 12:02:31 +0800

    assignment1修改

--- Assignment1/__init__.py
+++ Assignment1/__init__.py
@@ -1,9 +1,9 @@
-from part3.transformer_block import TransformerBlock
-from part3.causal_multi_head_attention_with_rope import CausalMultiHeadAttentionWithRoPE
-from part3.RMSnorm import RMSNorm
-from part3.SwiGLU import SwiGLU
-from part3.linear_and_embedding_module import LinearModule,EmbeddingModule
-from part3.scaled_dot_product_attention import ScaledDotProductAttention
-from part3.rope import RoPE
+from section3.transformer_block import TransformerBlock
+from section3.causal_multi_head_attention_with_rope import CausalMultiHeadAttentionWithRoPE
+from section3.RMSnorm import RMSNorm
+from section3.SwiGLU import SwiGLU
+from section3.linear_and_embedding_module import LinearModule,EmbeddingModule
+from section3.scaled_dot_product_attention import ScaledDotProductAttention
+from section3.rope import RoPE
 
 __all__ = ["TransformerBlock", "CausalMultiHeadAttentionWithRoPE", "RMSNorm", "SwiGLU", "LinearModule", "EmbeddingModule", "ScaledDotProductAttention", "RoPE"]
